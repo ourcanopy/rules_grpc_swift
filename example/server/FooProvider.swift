29b96ce6cb7797d94f3aea0dbd99d04e2df867d1
Jon Betti <jonbetti@gmail.com>
2018-01-28 17:11:11 -0500

    Run swiftformat

--- example/server/FooProvider.swift
+++ example/server/FooProvider.swift
@@ -2,8 +2,8 @@ import Dispatch
 import example_proto_Foo
 import Foundation
 
-class FooProvider : Foo_FooProvider {
-    func baz(request : Foo_Testy, session : Foo_FooBazSession) throws -> Foo_Testy {
+class FooProvider: Foo_FooProvider {
+    func baz(request: Foo_Testy, session _: Foo_FooBazSession) throws -> Foo_Testy {
         var response = Foo_Testy()
         response.foo = "hi from swift!"
         print("got request: \(request)")
